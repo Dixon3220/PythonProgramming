42b3cc033def986c04c0aee4896780425fb45bb8
dululusang <dululusang@outlook.com>
2021-08-28 11:31:14 +0800

    Log In Part - Bug Solved

--- origin/pythonprogrammingproject.py
+++ origin/pythonprogrammingproject.py
@@ -1,37 +1,13 @@
-# im Rose
+# Bug Solved
 
 import tkinter as tk
 import tkinter.messagebox
 import pickle
 
-window = tk.Tk()
-window.title('Python Expenses Recorder')
-window.geometry('500x300')  
-
-welcome_lable = tk.Label(window, 
-                         text='\n\nWelcome to Python Expenses Recorder!\n')
-welcome_lable.pack()
- 
-# Uesername
-usr_lable = tk.Label(window, text='Username')
-var_usr_name = tk.StringVar()
-entry_usr_name = tk.Entry(window, 
-                          textvariable=var_usr_name, font=('Arial', 14))
-usr_lable.pack()
-entry_usr_name.pack()
-
-# Password
-pwd_lable = tk.Label(window, text='Password')
-var_usr_pwd = tk.StringVar()
-entry_usr_pwd = tk.Entry(window, 
-                         textvariable=var_usr_pwd, font=('Arial', 14), show='*')
-pwd_lable.pack()
-entry_usr_pwd.pack()
-
 def usr_login():
     usr_name = var_usr_name.get()
-    usr_pwd = var_usr_pwd.get()
- 
+    usr_pwd = var_usr_pwd.get() 
+    
     try:
         with open('usrs_info.pickle', 'rb') as usr_file:
             usrs_info = pickle.load(usr_file)
@@ -40,7 +16,7 @@ def usr_login():
         with open('usrs_info.pickle', 'wb') as usr_file:
             usrs_info = {'python': 'python'}
             pickle.dump(usrs_info, usr_file)
-            usr_file.close()    
+            usr_file.close()  
  
     # See whether the username match in the file
     if usr_name in usrs_info:
@@ -63,48 +39,88 @@ def usr_sign_up():
         npf = new_pwd_confirm.get()
         nn = new_name.get()
 
-        with open('usrs_info.pickle', 'rb') as usr_file:
-            exist_usr_info = pickle.load(usr_file)
-        if np != npf:
-            tkinter.messagebox.showerror('Error', 'Password and confirm password must be the same!')
- 
+        try:
+            with open('usrs_info.pickle', 'rb') as usr_file:
+                exist_usr_info = pickle.load(usr_file)
+        except FileNotFoundError:
+            # if cannot find the user, creat a file with a Admin 'python'
+            with open('usrs_info.pickle', 'wb') as usr_file:
+                usrs_info = {'python': 'python'}
+                pickle.dump(usrs_info, usr_file)
+                usr_file.close()  
+        
         # If username already exit in the file 
-        elif nn in exist_usr_info:
+        if nn in exist_usr_info:
             tkinter.messagebox.showerror('Error', 'You has already signed up!')
  
-        else:
-            exist_usr_info[nn] = np
-            with open('usrs_info.pickle', 'wb') as usr_file:
-                pickle.dump(exist_usr_info, usr_file)
-            tkinter.messagebox.showinfo('Welcome', 'You have successfully signed up!')
-            window_sign_up.destroy()
+        elif np != npf:
+            tkinter.messagebox.showerror('Error', 'Password and confirm password must be the same!')
  
+        else:
+            if len(np) > 15:
+                tkinter.messagebox.showerror('Error', 'Password must be less than 15 characters!')
+            elif len(np) < 3:
+                tkinter.messagebox.showerror('Error', 'Password must be more than 3 characters')
+            else:
+                exist_usr_info[nn] = np
+                with open('usrs_info.pickle', 'wb') as usr_file:
+                    pickle.dump(exist_usr_info, usr_file)
+                    tkinter.messagebox.showinfo('Welcome', 'You have successfully signed up!')
+                    window_sign_up.destroy()
+
     window_sign_up = tk.Toplevel(window)
-    window_sign_up.geometry('300x200')
+    window_sign_up.geometry('400x300')
     window_sign_up.title('Sign up window')
  
     new_name = tk.StringVar() 
-    tk.Label(window_sign_up, text='Username').place(x=10, y=10) 
+    usr_sign = tk.Label(window_sign_up, text='Username')
+    usr_sign.pack()
     entry_new_name = tk.Entry(window_sign_up, textvariable=new_name)
-    entry_new_name.place(x=100, y=10)  
+    entry_new_name.pack()
  
     new_pwd = tk.StringVar()
-    tk.Label(window_sign_up, text='Password').place(x=10, y=50)
+    pwd_sign = tk.Label(window_sign_up, text='Password')
+    pwd_sign.pack()
     entry_usr_pwd = tk.Entry(window_sign_up, textvariable=new_pwd, show='*')
-    entry_usr_pwd.place(x=100, y=50)
+    entry_usr_pwd.pack()
  
     new_pwd_confirm = tk.StringVar()
-    tk.Label(window_sign_up, text='Confirm\npassword').place(x=10, y=80)
+    confirm_sign = tk.Label(window_sign_up, text='Confirm Password')
+    confirm_sign.pack()
     entry_usr_pwd_confirm = tk.Entry(window_sign_up, textvariable=new_pwd_confirm, show='*')
-    entry_usr_pwd_confirm.place(x=100, y=90)
+    entry_usr_pwd_confirm.pack()
  
-    btn_comfirm_sign_up = tk.Button(window_sign_up, text='Sign up', command=sign_to_python)
-    btn_comfirm_sign_up.place(x=180, y=120)
+    btn_confirm_sign_up = tk.Button(window_sign_up, text='Sign up', command=sign_to_python)
+    btn_confirm_sign_up.pack()
+
+window = tk.Tk()
+window.title('Python Expenses Recorder')
+window.geometry('500x300')  
+
+welcome_lable = tk.Label(window, text='\n\nWelcome to Python Expenses Recorder!\n')
+welcome_lable.pack()
+ 
+
+# Uesername
+usr_lable = tk.Label(window, text='Username')
+var_usr_name = tk.StringVar()
+entry_usr_name = tk.Entry(window, 
+                          textvariable=var_usr_name, font=('Arial', 14))
+usr_lable.pack()
+entry_usr_name.pack()
+
+# Password
+pwd_lable = tk.Label(window, text='Password')
+var_usr_pwd = tk.StringVar()
+entry_usr_pwd = tk.Entry(window, 
+                         textvariable=var_usr_pwd, font=('Arial', 14), show='*')
+pwd_lable.pack()
+entry_usr_pwd.pack() 
 
 # login and sign up button
 btn_login = tk.Button(window, text='Login', command=usr_login)
-btn_login.place(x=215, y=170)
+btn_login.pack()
 btn_sign_up = tk.Button(window, text='Sign up', command=usr_sign_up)
-btn_sign_up.place(x=207, y=200)
+btn_sign_up.pack()
 
 window.mainloop()
