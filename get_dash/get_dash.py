0d08ea148db4e16becfcdbb8e01b13371bbb142a
Dixon3220 <XLI077@e.ntu.edu.sg>
2021-09-03 15:11:30 +0800

    fix bugs in get_dash

--- get_dash/get_dash.py
+++ get_dash/get_dash.py
@@ -3,30 +3,31 @@ import tkinter as tk
 import tkinter.messagebox
 from tkinter import ttk
 import pickle
+import datetime
 from datetime import date
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from pandas import DataFrame
 
-
 main = tk.Tk()
 main.title('Main Menu')
 main.geometry('700x500')
 # read excel
-expense = pd.read_excel('expense.xlsx')  # [ignore this comment, only for testing] expense = pd.read_excel('D:\Python\Python_Programming\project\coding\PythonProgramming\get_dash\expense.xlsx')
-budget  = pd.read_excel('budget.xlsx')   # [ignore this comment, only for testing]  budget = pd.read_excel('D:\Python\Python_Programming\project\coding\PythonProgramming\get_dash\\budget.xlsx')
-
+expense = pd.read_excel(
+    'expense.xlsx')  # [ignore this comment, only for testing] expense = pd.read_excel('D:\Python\Python_Programming\project\coding\PythonProgramming\get_dash\expense.xlsx')
+budget = pd.read_excel(
+    'budget.xlsx')  # [ignore this comment, only for testing]  budget = pd.read_excel('D:\Python\Python_Programming\project\coding\PythonProgramming\get_dash\\budget.xlsx')
 
 # get information from the user, this could be changed in main, especially for month.
 today = date.today()
-today = '2021-08-30'               # testing value
+today = '2021-08-30'  # testing value
 month = str(today).split('-')[1]
 userid = 'Dixon3220'
 
 # get the data required
 user_expense = expense[(expense['userId'] == userid)].drop(['userId'], axis=1, inplace=False)
-user_budget  = budget [(budget ['userId'] == userid)].drop(['userId'], axis=1, inplace=False)
+user_budget = budget[(budget['userId'] == userid)].drop(['userId'], axis=1, inplace=False)
 
 
 def get_today_data(day):
@@ -44,7 +45,8 @@ def get_month_data(day):
 
     # get user's expense in this month
     year, month = day.split('-')[0], day.split('-')[1]
-    index = [int(str(x).split('-')[1]) == int(month) for x in user_expense['date'] if int(str(x).split('-')[0]) == int(year)]
+    index = [int(str(x).split('-')[1]) == int(month) for x in user_expense['date'] if
+             int(str(x).split('-')[0]) == int(year)]
     month_data = user_expense[index]
     return month_data
 
@@ -54,7 +56,7 @@ def get_year_data(day):
     global user_budget
 
     # get user's expense in this year
-    year= day.split('-')[0]
+    year = day.split('-')[0]
     index = [int(str(x).split('-')[0]) == int(year) for x in user_expense['date']]
     year_data = user_expense[index]
     return year_data
@@ -80,82 +82,47 @@ def get_plot_data(day, flag):
     # get data for the bar chart
     # flag = 'day' means getting bar chart data for this month, flag = 'month' means getting data for this year
     if flag == 'day':
-        this_expense = get_month_data(day)
-        thedate = this_expense['date']
-        date_name = thedate[thedate.duplicated(keep='first') == False]
+        this_day = datetime.datetime.strptime(today, "%Y-%m-%d")
+        weekday = this_day.weekday() + 1
+        this_week = [(this_day + datetime.timedelta(days=i)).strftime("%Y-%m-%d") for i in
+                     range(1 - weekday, 8 - weekday)]
         bar = {}
-        for key1 in date_name:
-            this_expense_day = this_expense[(this_expense['date'] == str(key1))]
-            bar[key1] = sum([float(amount) for amount in this_expense_day['amount']])
+        for this_date in this_week:
+            this_date_expense = user_expense.loc[(user_expense['date'] == this_date), 'amount']
+            bar[this_date] = sum([float(x) for x in this_date_expense])
+
     else:
         this_expense = get_year_data(day)
-        month_name = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Sep', 11:'Nov', 12:'Dec'}
+        month_name = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep',
+                      10: 'Sep', 11: 'Nov', 12: 'Dec'}
         bar = {}
-        for thismonth in range(12):
+        for thismonth in range(1, 13):
             index = [int(str(x).split('-')[1]) == thismonth for x in this_expense['date']]
             if not True in index:
-                bar[month_name[thismonth]] = 0
+                bar[thismonth] = 0
             else:
                 this_expense_month = this_expense[index]
-                bar[month_name[thismonth]] = sum([float(amount) for amount in this_expense_month['amount']])
+                bar[thismonth] = sum([float(amount) for amount in this_expense_month['amount']])
 
     return pie, bar
 
 
 def show():
-    global user_expense
-    global user_budget
-    global today
-
-    # get data
-    today_data = get_today_data(today)
-    today_total = sum([float(x) for x in today_data['amount']])
-    today_budget = 100                                                    # need to update
-    month_budget = 100                                                    # need to update
-    month_names = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', \
-                   9: 'September', 10: 'September', 11: 'November', 12: 'December'}
-    month_data = get_month_data(today)
-    month_total = sum([float(x) for x in month_data['amount']])
-    month_name = month_names[int(today.split('-')[1])]
-
-    # Textbox1
-    today_label = tk.Label(main, text='Today', font=('Arial', 13))
-    today_label.place(x=305, y=20)
-
-    textbox1 = tk.Text(main, height=4, width=30, font=("Arial", 10))
-    textbox1.insert("1.0", "Spend:  $" + str(today_total) + "\n")
-    textbox1.insert("2.0", "Budget:  $" + str(today_budget) + "\n")
-    textbox1.insert("3.0", "\n")
-    if today_total > today_budget:
-        textbox1.insert("4.0", "Over budget! Please be careful!\n")
-    else:
-        textbox1.insert("4.0", "Good job! Keep working!\n")
-
-    textbox1.place(x=220, y=50)
-
-    # Textbox2
-    today_label = tk.Label(main, text=month_name, font=('Arial', 13),padx=20)
-    today_label.place(x=530, y=20)
-
-    textbox2 = tk.Text(main, height=4, width=30, font=("Arial", 10))
-    textbox2.insert("1.0", "Spend:  $" + str(month_total)+ "\n")
-    textbox2.insert("2.0", "Budget:  $" + str(month_budget)+ "\n")
-    textbox2.insert("3.0", "\n")
-    if month_total > month_budget:
-        textbox2.insert("4.0", "Over budget! Please be careful!\n")
-    else:
-        textbox2.insert("4.0", "Good job! Keep working!\n")
-    textbox2.place(x=450, y=50)
-
-    # Display today's date
-    today_date = tk.Label(main, text='Date: ' + str(today) + '\n,  Username: ' + str(userid), font=("Arial", 13))
-    today_date.place(x=10, y=20)
-
     # Display logo
-    logo = tk.Canvas(main, height=60, width=60) #change size of logo
-    image_file = tk.PhotoImage(file='normal.gif') #change our logo here
+    logo = tk.Canvas(main, height=60, width=60)  # change size of logo
+    image_file = tk.PhotoImage(file="normal.gif")  # change our logo here
     image = logo.create_image(0, 0, anchor='nw', image=image_file)
     logo.place(x=50, y=70)
+
+    # Textbox2 title
+    month_names = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', \
+                   9: 'September', 10: 'September', 11: 'November', 12: 'December'}
+    month_name = month_names[int(today.split('-')[1])]
+    today_label = tk.Label(main, text=month_name, font=('Arial', 13))
+    today_label.place(x=540 - len(month_name) * 2, y=20)
+
+    # show detail data
+    show_detail()
 
     # all the functions buttons
     logout_button = tk.Button(main, text="Log out", width=13, command=logout)  # add command for the function
@@ -177,18 +144,74 @@ def show():
     def change_date(*arg):
         global today
         today = main_date.get()
-        show()
+        show_detail()
+        show_today_exp(today)
 
     date_all = user_expense['date']
-    option_list1 = date_all[date_all.duplicated(keep='first') == False]
+    option_list1 = list(date_all[date_all.duplicated(keep='first') == False])
+    option_list1.sort()
     main_date_value = tk.StringVar()
-    main_date = ttk.Combobox(main, width=8, textvariable=main_date_value)
+    main_date = ttk.Combobox(main, width=10, textvariable=main_date_value)
     main_date['values'] = tuple((x,) for x in option_list1)
     main_date.bind("<<ComboboxSelected>>", change_date)
     main_date.place(x=450, y=140)
     main_date_label = tk.Label(main, text='Date: ')
     main_date_label.place(x=400, y=140)
 
+
+def show_detail():
+    global user_expense
+    global user_budget
+    global today
+
+    # get data
+    today_data = get_today_data(today)
+    today_total = sum([float(x) for x in today_data['amount']])
+    today_budget = 100  # need to update
+    month_budget = 100  # need to update
+    month_names = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', \
+                   9: 'September', 10: 'September', 11: 'November', 12: 'December'}
+    month_data = get_month_data(today)
+    month_total = sum([float(x) for x in month_data['amount']])
+    month_name = month_names[int(today.split('-')[1])]
+
+    # Textbox1
+    today_label = tk.Label(main, text='Today', font=('Arial', 13))
+    today_label.place(x=305, y=20)
+
+    textbox1 = tk.Text(main, height=4, width=30, font=("Arial", 10))
+    textbox1.insert("1.0", "Spend:  $" + "{:.2f}".format(today_total) + "\n")
+    textbox1.insert("2.0", "Budget:  $" + "{:.2f}".format(today_budget) + "\n")
+    textbox1.insert("3.0", "\n")
+    if today_total > today_budget:
+        textbox1.insert("4.0", "Over budget! Please be careful!\n")
+    else:
+        textbox1.insert("4.0", "Good job! Keep working!\n")
+
+    textbox1.place(x=220, y=50)
+
+    # Textbox2
+
+    today_label = tk.Label(main, text="                                      ", font=('Arial', 13))
+    today_label.place(x=520, y=20)
+    today_label = tk.Label(main, text=month_name, font=('Arial', 13))
+    today_label.place(x=540 - len(month_name) * 2, y=20)
+
+    textbox2 = tk.Text(main, height=4, width=30, font=("Arial", 10))
+    textbox2.insert("1.0", "Spend:  $" + "{:.2f}".format(month_total) + "\n")
+    textbox2.insert("2.0", "Budget:  $" + "{:.2f}".format(month_budget) + "\n")
+    textbox2.insert("3.0", "\n")
+    if month_total > month_budget:
+        textbox2.insert("4.0", "Over budget! Please be careful!\n")
+    else:
+        textbox2.insert("4.0", "Good job! Keep working!\n")
+    textbox2.place(x=450, y=50)
+
+    # Display today's date
+    today_date = tk.Label(main, text='   Date: ' + str(today) + '\n   Username: ' + str(userid), font=("Arial", 12))
+    today_date.place(x=12, y=10)
+
+    # optionlist
     def change_type(*arg):
         if main_type.get() == 'Month':
             show_month_exp(str(today))
@@ -222,19 +245,19 @@ def show_today_exp(day):
     ax1 = figure1.add_subplot(111)
     bar1 = FigureCanvasTkAgg(figure1, main)
     bar1.get_tk_widget().place(x=300, y=170)
-    
+
     df1 = df1[['Date', 'Expense']].groupby('Date').sum()
     df1.plot(kind='bar', legend=True, ax=ax1)
     for i, v in enumerate(df1['Expense']):
-        ax1.text(i,v//2, str(v), ha= 'center')
+        ax1.text(i, v // 2, str(v), ha='center')
     ax1.axhline(50, linestyle='--', color='red')
-    ax1.set_xticklabels(data1['Date'],rotation=360)
+    ax1.set_xticklabels(data1['Date'], rotation=360)
     ax1.set_title('Date Vs. Expense')
 
     # Plot pie charts
     fig = Figure(figsize=(3, 2.7))  # create a figure object
     ax = fig.add_subplot(111)  # add an Axes to the figure
-    ax.pie(pie.values(), radius=1.2, labels=pie.keys(), autopct='%0.2f%%', shadow=True, )
+    ax.pie(pie.values(), radius=1.0, labels=pie.keys(), autopct='%0.2f%%', shadow=True, )
     chart1 = FigureCanvasTkAgg(fig, main)
     chart1.get_tk_widget().place(x=0, y=170)
 
@@ -248,7 +271,7 @@ def show_month_exp(day):
     bar_keys = [x for x in bar.keys()]
     bar_values = [y for y in bar.values()]
 
-    data1 = {'Date': bar_keys,
+    data1 = {'Months': bar_keys,
              'Expense': bar_values
              }
     df1 = DataFrame(data1, columns=['Months', 'Expense'])
@@ -257,9 +280,15 @@ def show_month_exp(day):
     bar1 = FigureCanvasTkAgg(figure1, main)
     bar1.get_tk_widget().place(x=300, y=180)
     df1 = df1[['Months', 'Expense']].groupby('Months').sum()
-    df1 = df1[bar_keys].groupby('Months').sum()
     df1.plot(kind='bar', legend=True, ax=ax1)
     ax1.set_title('Months Vs. Expense')
+
+    # Plot pie charts
+    fig = Figure(figsize=(3, 2.4))  # create a figure object
+    ax = fig.add_subplot(111)  # add an Axes to the figure
+    ax.pie(pie.values(), radius=1.0, labels=pie.keys(), autopct='%0.2f%%', shadow=True, )
+    chart1 = FigureCanvasTkAgg(fig, main)
+    chart1.get_tk_widget().place(x=0, y=180)
 
 
 # Change budget interface
@@ -318,13 +347,14 @@ def check_detail():
 
             user_expense.loc[(user_expense['type'] == type_old) & (user_expense['amount'] == amount_old) & \
                              (user_expense['date'] == today), ('type', 'amount')] = [edit_type, float(edit_amount)]
-            expense.loc[(expense['type'] == type_old) & (expense['amount'] == amount_old) & (expense['date'] == today) &\
+            expense.loc[(expense['type'] == type_old) & (expense['amount'] == amount_old) & (expense['date'] == today) & \
                         (expense['userId'] == userid), ('type', 'amount')] = [edit_type, float(edit_amount)]
             # user_expense['date'] = user_expense['date'].map(lambda x: str(x).split(' ')[0])
             user_expense.to_excel('user_expense.xlsx')
             expense.to_excel('expense.xlsx')
             s_window.destroy()
-            show()
+            show_detail()
+            show_today_exp(today)
 
         s_window = tk.Toplevel(detail_window)
         s_window.title('Edit')
@@ -340,13 +370,13 @@ def check_detail():
         # Type
         edit_type = tk.StringVar()
         edit_types = ttk.Combobox(s_window, width=18, textvariable=edit_type)
-        edit_types['values'] = ('food', 'entertainment', 'clothing', 'transportation', 'study', 'others')
+        edit_types['values'] = ('food', 'entertain', 'clothing', 'transport', 'study', 'others')
         edit_types.place(x=160, y=135)
         edit_type_label = tk.Label(s_window, text='Type: ')
         edit_type_label.place(x=90, y=135)
         # button
-        btn_yes = tk.Button(s_window, text='Confirm', width=10, command=\
-                            lambda: editdetail_yes(tList, s_window, edit_amount.get(), edit_type.get()))
+        btn_yes = tk.Button(s_window, text='Confirm', width=10, command= \
+            lambda: editdetail_yes(tList, s_window, edit_amount.get(), edit_type.get()))
         btn_yes.place(x=120, y=200)
         btn_no = tk.Button(s_window, text='Cancel', width=10, command=s_window.destroy)
         btn_no.place(x=220, y=200)
@@ -364,18 +394,19 @@ def check_detail():
             amount_old = float(str(detail_old).split(' ')[-1])
             tList.delete(tk.ACTIVE)
 
-            user_expense.drop(index=user_expense.loc[(user_expense['type'] == type_old) &\
-                                                       (user_expense['amount'] == float(amount_old)) &\
-                                                       (user_expense['date'] == today)].index, \
+            user_expense.drop(index=user_expense.loc[(user_expense['type'] == type_old) & \
+                                                     (user_expense['amount'] == float(amount_old)) & \
+                                                     (user_expense['date'] == today)].index, \
                               inplace=True)
             expense.drop(index=expense.loc[(expense['type'] == type_old) & \
                                            (expense['amount'] == float(amount_old)) & \
                                            (expense['date'] == today)].index, \
-                        inplace=True)
+                         inplace=True)
             user_expense.to_excel('user_expense.xlsx')
             expense.to_excel('expense.xlsx')
             s_window.destroy()
-            show()
+            show_detail()
+            show_today_exp(today)
 
         s_window = tk.Toplevel(detail_window)
         s_window.title('Remove')
@@ -396,12 +427,8 @@ def check_detail():
     detail_title.place(x=185, y=20)
 
     # Date
-    detail_date_type = tk.StringVar()
-    detail_date = ttk.Combobox(detail_window, width=8, textvariable=detail_date_type)
-    detail_date['values'] = tuple((x,) for x in user_expense['date'])
-    detail_date.place(x=380, y=60)
-    days_label = tk.Label(detail_window, text='Date: ')
-    days_label.place(x=340, y=60)
+    days_label = tk.Label(detail_window, text='Date:  ' + today, font=('Arial', 12))
+    days_label.place(x=320, y=65)
 
     # Detail Expense
     today_data = get_today_data(today)
@@ -421,7 +448,6 @@ def check_detail():
     # remove button
     tremovebutton = tk.Button(detail_window, text='remove', command=lambda: checkdetail_removedetail(tList))
     tremovebutton.place(x=280, y=280, width=100)
-
 
     # back button
     btn_back = tk.Button(detail_window, text='Back', width=13, command=detail_window.destroy)
@@ -454,7 +480,8 @@ def add_expenses():
         btn_addmore = tk.Button(s_window, text='Add More', width=10, command=s_window.destroy)
         btn_addmore.place(x=140, y=75)
 
-        show()
+        show_detail()
+        show_today_exp(today)
 
     add_window = tk.Toplevel(main)
     add_window.title('Add Expenses')
@@ -481,14 +508,15 @@ def add_expenses():
     # Type
     add_type = tk.StringVar()
     types = ttk.Combobox(add_window, width=18, textvariable=add_type)
-    types['values'] = ('food', 'entertainment', 'clothing', 'transportation', 'study', 'others')
+    types['values'] = ('food', 'entertain', 'clothing', 'transport', 'study', 'others')
     types.place(x=180, y=180)
     type_label = tk.Label(add_window, text='Type: ')
     type_label.place(x=130, y=180)
 
     # Confirm & back button
-    btn_add = tk.Button(add_window, text='Add', width=13, command= lambda: addexp_success(new_date.get(), \
-                                                                                          add_amount.get(), add_type.get()))
+    btn_add = tk.Button(add_window, text='Add', width=13, command=lambda: addexp_success(new_date.get(), \
+                                                                                         add_amount.get(),
+                                                                                         add_type.get()))
     btn_add.place(x=200, y=230)
     btn_back = tk.Button(add_window, text='Back', width=13, command=add_window.destroy)
     btn_back.place(x=20, y=20)
@@ -506,3 +534,4 @@ def logout():
 
 show()
 main.mainloop()
+
